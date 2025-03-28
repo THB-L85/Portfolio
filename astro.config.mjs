25dd21cb0a4e46a26592cccc51d8de3705c31a80
THB-L85 <luis.dr85a@gmail.com>
2025-03-27 20:37:38 -0600

    deploy prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thb-l85.github.io',
+  base: 'Porfolio',
   integrations: [tailwind()]
 });
