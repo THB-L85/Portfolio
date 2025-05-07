d2874e8e4dcb97ba46ef34739fd75e3162954f74
THB-L85 <luis.dr85a@gmail.com>
2025-05-06 18:35:55 -0600

    Fix: rename base in astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thb-l85.github.io',
-  base: 'Porfolio',
+  base: 'Portfolio',
   integrations: [tailwind()]
 });
